ad4a3753c902351e08053a44fa6c79866fc0a0be
Mukul Hase <mukulhase@gmail.com>
2019-05-18 23:26:17 +0530

    Logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: "Mukul Hase's Website",
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Personal website.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
